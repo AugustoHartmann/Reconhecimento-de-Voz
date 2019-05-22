c89d606197b325420f1ddd432233e76252d9fda7
Augusto Hartmann <48498100+AugustoHartmann@users.noreply.github.com>
2019-05-21 22:11:33 -0300

    Update Reconhecimento de Voz.py

--- Reconhecimento de Voz.py
+++ Reconhecimento de Voz.py
@@ -1,12 +1,14 @@
 pip install SpeechRecognition
 #baixar a versão adequada do PyAudio de {https://www.lfd.uci.edu/~gohlke/pythonlibs/#pyaudio}
+#--> Esta etapa é relativa a arquitetura do PC logo o download da Biblioteca PyAudio é feita de modo manual
 #pip install "diretório + nome do arquivo .whl"
 #https://medium.com/brasil-ai/reconhecimento-voz-python-35a5023767ca
 #pip install gTTS (serviço de transformação TextToSpeech)  
 #pip install playsound (toca sons)
-import speech_recognition as sr
 #from gtts import gTTS
 #from playsound import playsound
+
+import speech_recognition as sr
 
 def mic():
     microfone = sr.Recognizer()
